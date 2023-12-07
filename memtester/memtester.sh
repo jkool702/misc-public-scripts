005d9dbeea20ea89bd0697dce180066169602a49
jkool702 <107448833+jkool702@users.noreply.github.com>
2023-12-07 11:28:32 -0500

    memtester

--- memtester/memtester.sh
+++ memtester/memtester.sh
@@ -1,4 +1,6 @@
-runMemtesterPar() {
+#!/usr/bin/env bash
+
+runMemtester() {
 ## launches $(nproc) parallel memtester instances 
 #
 # (optional) input is the number of iterations. 
@@ -6,27 +8,31 @@ runMemtesterPar() {
 #
 # memory used per memtester instance is: (memTotal - unevictable - 1 GB) / nprocs
 #
-# Each memtester instance will print its stdout to a unique log file at ./memtester/memtester.${ID}.log
+# Each memtester instance will print its stdout/stderr to a unique log file at ./memtester/memtester.${ID}.std[out|err]log
 # Each instance will also send both its stdout and stderr to the stdout/stderr of the shell that forked them all.
 
 (
-	declare -a P;
+	local -a P
+	local -i iter memUse kk
 	mkdir -p ./memtester
+
+	iter=$1
 
 	# figure out how much memory to use (in mb)
 	memUse="$(( ( ( ( $(grep 'MemTotal' </proc/meminfo | sed -E s/'^.*\:[ \t]*([0-9]+) .*$'/'\1'/) ) - $(grep 'Unevictable' </proc/meminfo | sed -E s/'^.*\:[ \t]*([0-9]+) .*$'/'\1'/) ) - ( 2 ** 20 ) ) / ( 1024 * $(nproc) ) ))m"
 
 	# launch memtester instances
-	for kk in $(source /proc/self/fd/0 <<<"echo {1..$(nproc)}"); do
-		{ source /proc/self/fd/0; }<<EOF
-{ coproc p${kk} {
-memtester ${memUse} ${1} | tee -a >(cat >./memtester/memtester.${kk}.log)
-} 1>&\${fd1} 2>&\${fd2} 
-} 2>/dev/null
-P+=($p${kk}_PID)
-EOF
-	done;
+	for (( kk=1; kk<=$(nproc); kk++ )) do
 
+{ source /proc/self/fd/0; }<<<"{ coproc p${kk} {
+    memtester ${memUse} ${iter} 2>(tee >(cat >./memtester.${kk}.stderr.log) >&\${fd02}) | tee -a >(cat >./memtester/memtester.${kk}.stdout.log) >&\${fd1}
+  } {fd01}>&\${fd1} {fd02}>&\${fd2}
+} 2>/dev/null
+P+=(\${p${kk}_PID})"
+
+	done
+
+    # wait until they finish / are killed
 	wait "${P[@]}"
 	                                     
 	) {fd1}>&1 {fd2}>&2
