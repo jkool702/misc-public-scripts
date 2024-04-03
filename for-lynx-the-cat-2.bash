6ad1cb7dc6159bf7457f5209b8d6a47a1a1e06f4
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-04-03 19:44:33 -0400

    Update for-lynx-the-cat-2.bash

--- for-lynx-the-cat-2.bash
+++ for-lynx-the-cat-2.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 
+
 read_speedtest() (
 
 echo "BASE CASE: reading in variables directly from pipe"
@@ -115,6 +116,82 @@ echo "reading $nBytes bytes at a time (read -N)"
 
 printf '\n-------------------------------\n'
 done
+
+echo "reading using a tmpfile (forkruns method - requires inotifywait and fallocate)"
+
+    tdir=$(mktemp -d -p /dev/shm); : > ${tdir}/data
+    touch "${tdir}"/data
+( 
+    for nn in {100000000..100000500}; do 
+        linetype=$(( $RANDOM % 2 ));  
+        case $linetype in 
+            0) printf '%s a%s b%s c%s d%s\n' $linetype $nn $nn $nn $nn ;; 
+            1) printf '%s z%s y%s x%s w%s v%s u%s t%s s%s r%s q%s p%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn ;; 
+        esac; 
+        read -r -u $fdwait -t 0.02; 
+    done; 
+    touch "${tdir}/done"
+) {fdwait}<><(:) >"${tdir}"/data &
+    time { 
+    (
+        # attempt to truncate file(in 4096-byte blocks) every time this many lines are read
+        fallocate_check_nlines0=400
+        
+        # fork inotifywait to wattch tmpfile
+        inotifywait -m -e modify --format='\n' >&"$fd_wait" "${tdir}"/data 2>/dev/null &
+        pid_inotify="${!}"
+               # loop until processes are finished
+        stopFlag=false
+        until $stopFlag; do
+
+            # wait for inotifywait to tell you there is data
+            read -r -u "$fd_wait"
+
+            # read all the data you can. NOTE: no -t flag keeps the delimiter. Wont work with NULL delimiter.
+            mapfile -u "$fdr" A
+
+            # make sure you stopped reading data on a line break
+            [[ "${#A[@]}" == 0 ]] || [[ "${A[-1]: -1}" == $'\n' ]] || { read -r -u "$fdr"; A[-1]="${A[-1]}${REPLY}"$'\n'; }
+
+            # DO SOMETHING - non-vectorized version        
+            for nn in "${A[@]%$'\n'}"; do
+                case "${A[$kk]:0:1}" in 
+                    0) read -r dummy var1 dummy var2 dummy <<<"${A[$kk]}" ;;
+                    1) read -r dummy var1 dummy var2 dummy var3 dummy var4 dummy var5 dummy var6 <<<"${A[$kk]}" ;; 
+                esac; 
+            done
+
+            # end condition
+            [[ -f "${tdir}"/done1 ]] && [[ -f "${tdir}"/done2 ]] && [[ -f "${tdir}"/done3 ]] && [[ -f "${tdir}"/done4 ]] && [[ -f "${tdir}"/done5 ]] && stopFlag=true
+
+            # see if we should try asd truncate the file
+            fallocate_check_nlines+="${#A[@]}"
+            (( fallocate_check_nlines >= fallocate_check_nlines0 )) && {
+                # reset line counter
+                fallocate_check_nlines=0
+                
+                # get read fd byte offset from procfs
+                read -r </proc/self/fdinfo/$fdr
+                byte_pos_cur="${REPLY#*$'\t'}"
+
+                # remove data from the start of the file in 4096 byte blocks, up to but not past curren read fd byte offset
+                (( ( byte_pos_cur - byte_pos_last ) > 4096 )) && {
+                    bytes_rm="$(( 4096 * ( byte_pos_cur / 4096 ) - byte_pos_last ))"
+                    fallocate -p -o ${byte_pos_cur} -l "$bytes_rm" "${tdir}"/data && byte_pos_last+="$bytes_rm"
+                }
+            }
+        done
+
+        # kill inotifywait
+        kill -9 "$pid_inotify"
+    ) {fdr}<${tdir}/data {fd_wait0}<><(:) {fd_wait}<><(:)
+
+    # rm tmp dir
+    \rm -r $tdir
+    }  
+) 
+
+printf '\n-------------------------------\n'
 )
 
 read_speedtest
