61ba62fd65ab1fd65faa0d10078ef38fedfbd508
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-04-03 19:32:20 -0400

    Update for-lynx-the-cat-2.bash

--- for-lynx-the-cat-2.bash
+++ for-lynx-the-cat-2.bash
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+
 read_speedtest() (
 
-echo "BASE CASE: reading direct from pipe"
+echo "BASE CASE: reading in variables directly from pipe"
 ( 
-    for nn in {100000000000..100000000100}; do 
+    for nn in {100000000..100000500}; do 
         linetype=$(( $RANDOM % 2 ));  
         case $linetype in 
             0) printf '%s a%s b%s c%s d%s\n' $linetype $nn $nn $nn $nn ;; 
-            1) printf '%s z%s y%s x%s w%s v%s u%s t%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn ;; 
+            1) printf '%s z%s y%s x%s w%s v%s u%s t%s s%s r%s q%s p%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn ;; 
         esac; 
         read -r -u $fdwait -t 0.02; 
     done; 
@@ -18,8 +19,33 @@ echo "BASE CASE: reading direct from pipe"
             read -r -d ' ' -u "$fd" || break
             case "${REPLY}" in 
                 0) read -r -u "$fd" var1 dummy var2 dummy ;;
-                1) read -r -u "$fd" var1 dummy var2 dummy var3 dummy var4 <<<"${A[$kk]}" ;; 
+                1) read -r -u "$fd" var1 dummy var2 dummy var3 dummy var4 dummy var5 dummy var6<<<"${A[$kk]}" ;; 
             esac; 
+        done;
+    }  
+) {fd}<&0
+
+printf '\n-------------------------------\n'
+
+
+echo "BASE CASE (alt): reading in variables directly from pipe to an array"
+( 
+    for nn in {100000000..100000500}; do 
+        linetype=$(( $RANDOM % 2 ));  
+        case $linetype in 
+            0) printf '%s a%s b%s c%s d%s\n' $linetype $nn $nn $nn $nn ;; 
+            1) printf '%s z%s y%s x%s w%s v%s u%s t%s s%s r%s q%s p%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn ;; 
+        esac; 
+        read -r -u $fdwait -t 0.02; 
+    done; 
+) {fdwait}<><(:) | ( 
+    time { 
+        while true; do
+            read -r -u "$fd" -a A || break
+            case "${A[0]}" in 
+                0) var1="${A[1]}" var3="${A[3]}" ;;
+                1) var1="${A[1]}" var3="${A[3]}" var3="${A[3]}" var5="${A[5]}" var6="${A[6]}" ;; 
+            esac 
         done;
     }  
 ) {fd}<&0
@@ -30,11 +56,11 @@ printf '\n-------------------------------\n'
 for nLines in {1..10} {12..20..2} {24..40..4}; do
 echo "reading $nLines lines at a time (mapfile)"
 ( 
-    for nn in {100000000000..100000000100}; do 
+    for nn in {100000000..100000500}; do 
         linetype=$(( $RANDOM % 2 ));  
         case $linetype in 
             0) printf '%s a%s b%s c%s d%s\n' $linetype $nn $nn $nn $nn ;; 
-            1) printf '%s z%s y%s x%s w%s v%s u%s t%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn ;; 
+            1) printf '%s z%s y%s x%s w%s v%s u%s t%s s%s r%s q%s p%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn ;; 
         esac; 
         read -r -u $fdwait -t 0.02; 
     done; 
@@ -46,7 +72,7 @@ echo "reading $nLines lines at a time (mapfile)"
             for kk in "${!A[@]}"; do 
                 case "${A[$kk]:0:1}" in 
                     0) read -r dummy var1 dummy var2 dummy <<<"${A[$kk]}" ;;
-                    1) read -r dummy var1 dummy var2 dummy var3 dummy var4 <<<"${A[$kk]}" ;; 
+                    1) read -r dummy var1 dummy var2 dummy var3 dummy var4 dummy var5 dummy var6 <<<"${A[$kk]}" ;; 
                 esac; 
             done; 
         done;
@@ -61,11 +87,11 @@ for nBytes in {100..1000..50}; do
     
 echo "reading $nBytes bytes at a time (read -N)"
 ( 
-    for nn in {100000000000..100000000100}; do 
+    for nn in {100000000..100000500}; do 
         linetype=$(( $RANDOM % 2 ));  
         case $linetype in 
             0) printf '%s a%s b%s c%s d%s\n' $linetype $nn $nn $nn $nn ;; 
-            1) printf '%s z%s y%s x%s w%s v%s u%s t%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn ;; 
+            1) printf '%s z%s y%s x%s w%s v%s u%s t%s s%s r%s q%s p%s\n' $linetype $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn $nn ;; 
         esac; 
         read -r -u $fdwait -t 0.02; 
     done; 
@@ -79,7 +105,7 @@ echo "reading $nBytes bytes at a time (read -N)"
                     read -r -d ' ' || break
                     case "${REPLY}" in 
                         0) read -r var1 dummy var2 dummy ;;
-                        1) read -r var1 dummy var2 dummy var3 dummy var4 <<<"${A[$kk]}" ;; 
+                        1) read -r var1 dummy var2 dummy var3 dummy var4 dummy var5 dummy var6<<<"${A[$kk]}" ;; 
                     esac; 
                 done;
             } <<<"${x}${y}"
