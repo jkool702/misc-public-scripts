fcfc3296d3ab7ed1ef9bb84cd6c5666c88d6d2dc
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-04-03 20:14:42 -0400

    Update for-lynx-the-cat-2.bash

--- for-lynx-the-cat-2.bash
+++ for-lynx-the-cat-2.bash
@@ -187,3 +187,303 @@ done
 )
 
 read_speedtest
+
+# RESULTS
+
+:<<'EOF'
+
+BASE CASE: reading in variables directly from pipe
+
+real    0m5.108s
+user    0m0.114s
+sys     0m0.068s
+
+-------------------------------
+BASE CASE (alt): reading in variables directly from pipe to an array
+
+real    0m5.106s
+user    0m0.111s
+sys     0m0.061s
+
+-------------------------------
+reading using a tmpfile (forkruns method - requires inotifywait and fallocate)
+
+real    0m5.119s
+user    0m0.162s
+sys     0m0.041s
+
+-------------------------------
+reading 1 lines at a time (mapfile)
+
+real    0m5.104s
+user    0m0.179s
+sys     0m0.124s
+
+-------------------------------
+reading 2 lines at a time (mapfile)
+
+real    0m5.106s
+user    0m0.176s
+sys     0m0.125s
+
+-------------------------------
+reading 3 lines at a time (mapfile)
+
+real    0m5.106s
+user    0m0.189s
+sys     0m0.111s
+
+-------------------------------
+reading 4 lines at a time (mapfile)
+
+real    0m5.107s
+user    0m0.166s
+sys     0m0.130s
+
+-------------------------------
+reading 5 lines at a time (mapfile)
+
+real    0m5.106s
+user    0m0.171s
+sys     0m0.117s
+
+-------------------------------
+reading 6 lines at a time (mapfile)
+
+real    0m5.108s
+user    0m0.185s
+sys     0m0.114s
+
+-------------------------------
+reading 7 lines at a time (mapfile)
+
+real    0m5.107s
+user    0m0.161s
+sys     0m0.128s
+
+-------------------------------
+reading 8 lines at a time (mapfile)
+
+real    0m5.109s
+user    0m0.155s
+sys     0m0.139s
+
+-------------------------------
+reading 9 lines at a time (mapfile)
+
+real    0m5.109s
+user    0m0.171s
+sys     0m0.119s
+
+-------------------------------
+reading 10 lines at a time (mapfile)
+
+real    0m5.105s
+user    0m0.173s
+sys     0m0.111s
+
+-------------------------------
+reading 12 lines at a time (mapfile)
+
+real    0m5.110s
+user    0m0.146s
+sys     0m0.135s
+
+-------------------------------
+reading 14 lines at a time (mapfile)
+
+real    0m5.107s
+user    0m0.140s
+sys     0m0.134s
+
+-------------------------------
+reading 16 lines at a time (mapfile)
+
+real    0m5.108s
+user    0m0.159s
+sys     0m0.119s
+
+-------------------------------
+reading 18 lines at a time (mapfile)
+
+real    0m5.109s
+user    0m0.142s
+sys     0m0.127s
+
+-------------------------------
+reading 20 lines at a time (mapfile)
+
+real    0m5.106s
+user    0m0.150s
+sys     0m0.115s
+
+-------------------------------
+reading 24 lines at a time (mapfile)
+
+real    0m5.113s
+user    0m0.149s
+sys     0m0.111s
+
+-------------------------------
+reading 28 lines at a time (mapfile)
+
+real    0m5.113s
+user    0m0.143s
+sys     0m0.114s
+
+-------------------------------
+reading 32 lines at a time (mapfile)
+
+real    0m5.113s
+user    0m0.149s
+sys     0m0.095s
+
+-------------------------------
+reading 36 lines at a time (mapfile)
+
+real    0m5.115s
+user    0m0.122s
+sys     0m0.112s
+
+-------------------------------
+reading 40 lines at a time (mapfile)
+
+real    0m5.111s
+user    0m0.114s
+sys     0m0.113s
+
+-------------------------------
+reading 100 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.198s
+sys     0m0.093s
+
+-------------------------------
+reading 150 bytes at a time (read -N)
+
+real    0m5.106s
+user    0m0.226s
+sys     0m0.086s
+
+-------------------------------
+reading 200 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.211s
+sys     0m0.086s
+
+-------------------------------
+reading 250 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.202s
+sys     0m0.082s
+
+-------------------------------
+reading 300 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.201s
+sys     0m0.089s
+
+-------------------------------
+reading 350 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.197s
+sys     0m0.082s
+
+-------------------------------
+reading 400 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.197s
+sys     0m0.077s
+
+-------------------------------
+reading 450 bytes at a time (read -N)
+
+real    0m5.106s
+user    0m0.210s
+sys     0m0.076s
+
+-------------------------------
+reading 500 bytes at a time (read -N)
+
+real    0m5.106s
+user    0m0.198s
+sys     0m0.073s
+
+-------------------------------
+reading 550 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.191s
+sys     0m0.071s
+
+-------------------------------
+reading 600 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.171s
+sys     0m0.092s
+
+-------------------------------
+reading 650 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.174s
+sys     0m0.082s
+
+-------------------------------
+reading 700 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.196s
+sys     0m0.072s
+
+-------------------------------
+reading 750 bytes at a time (read -N)
+
+real    0m5.106s
+user    0m0.179s
+sys     0m0.074s
+
+-------------------------------
+reading 800 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.191s
+sys     0m0.066s
+
+-------------------------------
+reading 850 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.187s
+sys     0m0.075s
+
+-------------------------------
+reading 900 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.183s
+sys     0m0.064s
+
+-------------------------------
+reading 950 bytes at a time (read -N)
+
+real    0m5.105s
+user    0m0.168s
+sys     0m0.072s
+
+-------------------------------
+reading 1000 bytes at a time (read -N)
+
+real    0m5.107s
+user    0m0.187s
+sys     0m0.055s
+
+-------------------------------
+EOF
