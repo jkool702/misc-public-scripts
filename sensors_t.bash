30c8083f1066a6683bd40b073ae29b4bcc79e293
jkool702 <107448833+jkool702@users.noreply.github.com>
2024-10-26 18:34:24 -0400

    Update sensors_t.bash

--- sensors_t.bash
+++ sensors_t.bash
@@ -5,11 +5,11 @@ sensors_t() {
 # 
 # USAGE: sensors_t [N]
 #     N: length of time to sleep between updates in seconds. DEFAULT is N=1
-#  NOTE: sensors_t is a bash function. sensos_t.bash must be sourced before sensors_t can be used
+#  NOTE: "sensors_t" is a bash function. sensos_t.bash must be sourced before sensors_t can be used
 #
-# IF AVAILABLE: liquidctl and nvidia-smi will be used to display AIO temps and nvidia GPOU temps
+# IF AVAILABLE: nvidia-smi will be used to display nvidia GPU temps
 #
-# DEPENDENCIES: bash 4+, sensors (from lm_sensors), sed, grep. tail, head
+# DEPENDENCIES: bash 4+, sensors (from lm_sensors), sed, grep, tail, head
 
     # check for sensors support
     sensors &>/dev/null || sensors-detect || {
@@ -19,8 +19,8 @@ sensors_t() {
 
     (
         # make vars local
-        local -a t_max t_cur
-        local tmpStr t_max_cpu nn nvidiaFlag liquidctlFlag liquidctlName
+        local -a t_max t_cur t_cur_cpu
+        local tmpStr t_max_cpu g_cur nn nvidiaFlag
         local -i kk sleepTime
 
         # get how long to sleep between 
@@ -32,24 +32,30 @@ sensors_t() {
         # reset bash timer
         SECONDS=0
 
-        # determine if we have nvidia-smi and/or liquidctl
+        # determine if we have nvidia-smi 
         type nvidia-smi &>/dev/null && nvidiaFlag=true || nvidiaFlag=false
-        type liquidctl &>/dev/null && liquidctlFlag=true || liquidctlFlag=false
-        ${liquidctlFlag} && liquidctlName="$(printf '\nAIO: %s\n' "$(liquidctl list | head -n 1 | sed -E s/'^.*\:'//)";)"
 
         # start main loop
         while true; do
 
             # get CPU (and GPU) temps afrom sensors and tweak the output to display how we want
-            tmpStr="$(${nvidiaFlag} && printf 'GPU TEMP:      +%s  ( GPU MAX = %%s )\n' "$(nvidia-smi | grep -oE '[0-9]+C' | sed -E s/'C'/'°C'/)"; sensors | grep -iE "core|package"  | grep -vF "coretemp-isa-0000" | sed -E 's/\(.*$/\( MAX = %s )/; s/\.0//g')"; 
+            tmpStr="$(printf '\n----------------\n'; sensors | grep -B 1 -E '°C|RPM|Adapter' | grep -E '^[^[:space:]]' | sed -E 's/\-\-/\n----------------/; s/Adapter.*$/----------------/;s/°C[[:space:]]*(\(.*)?$/°C  \( MAX = %s )/')"; 
 
             # extract the actual temps from the full text output
-            mapfile -t t_cur < <(sed -E 's/^[^+-]*([+-])/\1/; s/C.*$/C/' <<<"${tmpStr}");
+            mapfile -t t_cur < <(grep -F '%s'  <<<"${tmpStr}" | sed -E 's/^[^+-]*([+-])/\1/; s/C.*$/C/');
+            mapfile -t t_cur_cpu < <(grep -F '%s'  <<<"${tmpStr}" | grep -E '^(Package|Core)' | sed -E 's/^[^+-]*([+-])/\1/; s/C.*$/C/');
 
             # get current max CPU temp and add it to the output string / array of current temps
-            t_max_cpu="$(IFS=$'\n'; sort -n <<<"${t_cur[*]:$(${nvidiaFlag} && echo '1' || echo '0')}" | tail -n 1)"; 
-            tmpStr+=$'\n''CORE MAX:      '"${t_max_cpu}"'  ( CPU MAX = %s )'$'\n'; 
+            t_max_cpu="$(IFS=$'\n'; sort -n <<<"${t_cur_cpu[*]}" | tail -n 1)"; 
+            tmpStr+=$'\n\n''----------------'$'\n''----------------'$'\n\n''CPU HOT TEMP: '"${t_max_cpu}"'  ( CPU HOT MAX = %s )'$'\n'; 
             t_cur+=("${t_max_cpu}"); 
+
+            # get GPU temp
+            ${nvidiaFlag} && {
+                g_cur="$(nvidia-smi | grep -oE '[0-9]+C' | sed -E s/'C'/'°C'/)"
+                tmpStr+=$'\n''GPU TEMP:     +'"${g_cur}"'  ( GPU MAX = %s )'$'\n'
+                t_cur+=("${g_cur}");
+            } 
 
             # update the maximum recorded temperature for each sensor if current temp is higher than recorded maximum temp
             [[ ${#t_max[@]} == 0 ]] && t_max=("${t_cur[@]}") || for nn in "${!t_cur[@]}"; do 
@@ -60,13 +66,7 @@ sensors_t() {
             printf '||---------------------------------------||\n\nMonitor has been running for:  %s seconds\n\n' "${SECONDS}"
 
             # print CPU (and GPU) temps
-            ${nvidiaFlag} && tmpStr="${tmpStr/$'\n'/$'\n\n'}"
-            printf "${tmpStr}" "${t_max[@]}"
-
-            # if we have liquidctl print AIO temps and pump speed
-            ${liquidctlFlag} && { printf '%s\n' "${liquidctlName}"; liquidctl status | grep -iE "pump|temp"; }
-
-            printf '\n';
+            printf "${tmpStr}"'\n' "${t_max[@]}"
 
             # sleep for sleepTime seconds
             read -r -u ${fd_sleep} -t ${sleepTime}
