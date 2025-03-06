7a47f036f8107f53f37b7ac6c2b3f053baf3fea4
jkool702 <107448833+jkool702@users.noreply.github.com>
2025-03-06 01:01:49 -0500

    Update bashCmdTime.bash

--- bashCmdTime.bash
+++ bashCmdTime.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+shopt -s extglob
+
 bashCmdTime() (
     ## efficiently produces an accurate per-command execution time profile for shell scripts and functions using a DEBUG trap
     #
@@ -12,7 +14,9 @@ bashCmdTime() (
     # DEPENDENCIES:
     #   1) a recent-ish bash versioin (4.0+ (???) - it needs to support the $EPOCHREALTIME variable)
     #   2) sed, grep, sort, mkdir
-    
+
+    shopt -s extglob
+
     bashCmdTime_TMPDIR=''
     
     [[ -d /dev/shm ]] && { 
@@ -20,7 +24,7 @@ bashCmdTime() (
         until ! [[ -d "$bashCmdTime_TMPDIR" ]]; do
             bashCmdTime_TMPDIR=/dev/shm/.bash.cmd.time."$(printf '%X' ${RANDOM}${RANDOM:1})"
         done
-        mkdir -p "$bashCmdTime_TMPDIR" && export bashCmdTime_TMPDIR="$bashCmdTime_TMPDIR" || bashCmdTime_TMPDIR=''
+        mkdir -p "$bashCmdTime_TMPDIR" || bashCmdTime_TMPDIR=''
     }
     
     [[ "$bashCmdTime_TMPDIR" ]] || {
@@ -28,22 +32,30 @@ bashCmdTime() (
         until ! [[ -d "$bashCmdTime_TMPDIR" ]]; do
             bashCmdTime_TMPDIR=/tmp/.bash.cmd.time."$(printf '%X' ${RANDOM}${RANDOM:1})"
         done        
-        mkdir -p "$bashCmdTime_TMPDIR" && export bashCmdTime_TMPDIR="$bashCmdTime_TMPDIR" || bashCmdTime_TMPDIR=''
+        mkdir -p "$bashCmdTime_TMPDIR" || bashCmdTime_TMPDIR=''
     }
     
     [[ "$bashCmdTime_TMPDIR" ]] || {
-        bashCmdTime_TMPDIR="$PWD/.bash.cmd.time"
-        mkdir -p "$bashCmdTime_TMPDIR" && export bashCmdTime_TMPDIR="$PWD/.bash.cmd.time"
+        bashCmdTime_TMPDIR="$PWD/.bash.cmd.time.$(printf '%X' ${RANDOM}${RANDOM:1})"
+        until ! [[ -d "$bashCmdTime_TMPDIR" ]]; do
+            bashCmdTime_TMPDIR="$PWD/.bash.cmd.time.$(printf '%X' ${RANDOM}${RANDOM:1})"
+        done   
+        mkdir -p "$bashCmdTime_TMPDIR" || bashCmdTime_TMPDIR=''
+    }
+
+     [[ "$bashCmdTime_TMPDIR" ]] || {
+         printf '\nERROR: could not create a tmpdir under /dev/shm nor /tmp nor PWD (%s). \nPlease ensure you have requisite write permissions in one of these directories. ABORTING\n\n' "$PWD"
+         return 1
     }
     
-    bashCmdTime_LOGFILE:="${bashCmdTime_TMPDIR}"/time.ALL
+    bashCmdTime_LOGFILE="${bashCmdTime_TMPDIR}"/time.ALL
 
    
 getTimeDiff () {
-        local d d6;
-        printf -v d '%.07d' $(( ${2//./} - ${1//./} ));
-        d6=$(( ${#d} - 6 ));
-        printf '%s.%s\n' "${d:0:$d6}" "${d:$d6}"
+    local d d6;
+    printf -v d '%.07d' $(( ${2//./} - ${1//./} ));
+    d6=$(( ${#d} - 6 ));
+    printf '%s.%s\n' "${d:0:$d6}" "${d:$d6}"
 };
 
 printTimeDiff() {
@@ -54,13 +66,15 @@ printTimeDiff() {
     
     tEnd="$(<"${6}")"
     [[ $tEnd ]] || tEnd="${EPOCHREALTIME}"
+
+    { [[ $tStart ]] && [[ $tEnd ]]; } || return 1
     
     printf '[%s] {%s} %s (%s):  %s sec  (%s --> %s)\n' "$1" "$2" "$3" "${4//$'\n'/'$'"'"'\n'"'"}" "$(getTimeDiff "$tStart" "$tEnd")" "$tStart" "$tEnd"
 }
 export -f getTimeDiff
 export -f printTimeDiff    
 
-   if [[ -t 0 ]]; then
+    if [[ -t 0 ]]; then
         runCmd="${@}"
     else
         runCmd="cat | { ${@}; }"
@@ -93,9 +107,9 @@ echo \"\$EPOCHREALTIME\" >\"${bashCmdTime_TMPDIR}\"/.bash.cmd.time.start.\$BASHP
 ${runCmd}
 
 ) {fd_bashCmdTime}>${bashCmdTime_LOGFILE}"
-	
+    
 eval "${runFuncSrc}"
-	
+    
 . <(declare -f runFunc)
 
 runFunc
@@ -103,26 +117,22 @@ runFunc
 mapfile -t uniq_lines < <(grep -E '^\[[0-9]' "${bashCmdTime_LOGFILE}" | sed -E s/'(^\[[0-9]+\] \{[0-9]+\} [0-9]+) .*$'/'\1'/ | sort -k1,3 -u)
 mapfile -t uniq_pids < <(printf '%s\n' "${uniq_lines[@]%% *}" | sort -u | sed -E 's/\[//;s/\]//')
 
-uniq_lines_pid=()
-
 for p in "${uniq_pids[@]}"; do
     grep -E '^\['"$p"  "${bashCmdTime_LOGFILE}" >"${bashCmdTime_TMPDIR}"/time.$p
-    mapfile -t uniq_lines_pid < <(printf '%s\n' "${uniq_lines[@]}" | grep -E'^\['"$p")
+    mapfile -t uniq_lines_pid < <(printf '%s\n' "${uniq_lines[@]}" | grep -E '^\['"$p")
     : >"${bashCmdTime_TMPDIR}"/time.combined.$p
     for l in "${uniq_lines_pid[@]}"; do
-        linesCmdCur="$(grep -F "$l" "${bashCmdTime_TMPDIR}"/time.$p)"
-	timesCmdCur=("${linesCmdCur[@]##*:  }")
+        mapfile -t linesCmdCur < <(grep -F "$l" "${bashCmdTime_TMPDIR}"/time.$p)
+        timesCmdCur=("${linesCmdCur[@]##*:  }")
         timesCmdCur=("${timesCmdCur[@]%% sec*}")
-	IFS0="$IFS"
-	IFS='+'
-        tSum=$(( "${timesCmdCur[*]//./}" ))
-	IFS="$IFS0"
+        timesCmdCur=("${timesCmdCur[@]//./}")
+        tSum="$(IFS='+'; printf '%.07d' "$(( "${timesCmdCur[*]##*(0)}" ))")"
         t6=$(( ${#tSum} - 6 ))
-	printf '%s:  %s.%s\n' "${linesCmdCur[0]%%:  *}" "${tSum:0:$t6}" "${tSum:$t6}" >>"${bashCmdTime_TMPDIR}"/time.combined.$p
+        printf '%s:  %s.%s\n' "${linesCmdCur[0]%%:  *}" "${tSum:0:$t6}" "${tSum:$t6}" >>"${bashCmdTime_TMPDIR}"/time.combined.$p
     done
 done
 
-printf '\nVarious time profiles can be found under %s\n\n' "${bashCmdTime_TMPDIR}"
+printf '\nVarious time profiles can be found under %s\n\n' "${bashCmdTime_TMPDIR}" >&2
 
 export -n bashCmdTime_TMPDIR
 export -nf getTimeDiff
